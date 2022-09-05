2faee42cb42cb6cede977d5c53a7b977061109a1
Quimari <Queexn.Pari@gmail.com>
2022-09-05 00:23:37 -0400

    commit tree 4fbc46e8d52b6607c622c2e8f4fe53aacebfba8d

---
+++ index.js
@@ -0,0 +1,44 @@
+
+function add() {
+
+} 
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+function divide () {
+
+} function add(a, b) {
+    return a + b
+} console.log (600, 26)
+
+function subtract(a, b) {
+    return a - b 
+} console.log(170, 340)
+
+function multiply(a, b) {
+    return a * b 
+} console.log(17447, 4)
+
+function divide(a, b) {
+    return a / b
+} console.log(4.9236111112, 2)
+
+
+function increment(n) {  
+ return (n+=1)
+    } 
+
+function decrement(n) {
+    return (n-=1)
+}
+
+function makeInt(n) {
+    return parseInt( n, 10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n)
+}
